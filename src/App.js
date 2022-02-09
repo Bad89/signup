668e6185e74a7e6b7b233ee0b093312a84bbd8d3
bad89 <badeailham@gmail.com>
2022-02-09 09:47:10 +0700

    livechatfbinstalling

--- src/App.js
+++ src/App.js
@@ -10,10 +10,11 @@ function App() {
     <>
       <Router>
         <Navbar />
+        <MessengerCustomerChat pageId="100077654544538" appId="481482256871485" />
         <Switch>
           <Route path="/" exact component={Home} />
         </Switch>
-        <MessengerCustomerChat pageId="100077654544538" appId="481482256871485" />
+        
       </Router>
       
     </>
