d47f0b33e0dbcc87460fe59ef412fb93eb359753
bad89 <badeailham@gmail.com>
2022-02-09 15:58:56 +0700

    updateresponsive1

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
 import React from "react";
-import { useMediaQuery } from 'react-responsive';
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
-import Home from "./components/pages/Home";
+
+import Responsive from './Responsive';
 
 
 
-const App= () => {
-  const isDesktopOrLaptop = useMediaQuery({
-    query: '(min-width: 1224px)'
-  })
-  const BigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
-  const TabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
-  const Portrait = useMediaQuery({ query: '(orientation: portrait)' })
-  const Retina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
 
-  return <div>
+
+
+
+
+
+
+function App () {
+  return (
+
+   
+
     <>
     
 
+<Responsive />
+      
 
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
-    </Router>
+     
+      
+    </>
 
-   
-    
-  </>
-  </div>
+     
+
+  );
 }
-
-
-
 
 export default App;
---
+++ src/Responsive.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useMediaQuery } from 'react-responsive'
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import "./App.css";
+
+const Responsive = () => {
+    const isDesktopOrLaptop = useMediaQuery({
+      query: '(min-width: 1224px)'
+    })
+    const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+    const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+    const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
+  
+    return <div>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  }
+
+  export default Responsive;
