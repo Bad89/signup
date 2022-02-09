f8a108e31a4ed54b2309906f001960b92a580e71
bad89 <badeailham@gmail.com>
2022-02-09 15:35:53 +0700

    updateview1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useMediaQuery } from 'react-responsive';
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
@@ -6,30 +7,34 @@ import Home from "./components/pages/Home";
 
 
 
+const App= () => {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 1224px)'
+  })
+  const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
 
-function App () {
-  return (
-
-   
-
+  return <div>
     <>
     
 
 
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-        </Switch>
-      </Router>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
 
-     
-      
-    </>
-
-     
-
-  );
+   
+    
+  </>
+  </div>
 }
+
+
+
 
 export default App;
