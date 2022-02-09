aae1e74afc2c7f0de35401d57f69dd0952a25887
bad89 <badeailham@gmail.com>
2022-02-09 08:57:31 +0700

    updatefblivechat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
-import Home from './components/pages/Home';
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/pages/Home";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function App() {
   return (
-    <> 
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+        <MessengerCustomerChat pageId="100077654544538" appId="481482256871485" />
       </Router>
+      
     </>
-   
   );
 }
-
-
-
-
 
 export default App;
