5ff7b95d3f9b8c158a66f14afb26460f1a5b615d
bad89 <badeailham@gmail.com>
2022-02-10 03:49:33 +0700

    changechat

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/pages/Home";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
+        <MessengerCustomerChat pageId="101589709123007" appId="481482256871485" />
         <Switch>
           <Route path="/" exact component={Home} />
         </Switch>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,14 +2,14 @@ import React from "react";
 import "../../App.css";
 import HeroSection from "../HeroSection";
 import Cards from '../Cards';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+
 
 function Home() {
   return (
     <>
       <HeroSection />
       <Cards />
-      <MessengerCustomerChat pageId="101589709123007" appId="481482256871485" />
+      
     </>
   );
 }
