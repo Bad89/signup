94761c110790329ac1e18cb72a08b70bdd362557
bad89 <badeailham@gmail.com>
2022-02-09 18:32:53 +0700

    updatecardlabel

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -12,19 +12,19 @@ function Cards() {
             <CardItem
               src="images/dog-beach-essentials.jpg"
               text="Explore item for your pet on store"
-              label="Pet Food, Item, Medicine"
+              label="Pet Food, Item, Medicine upComing"
               path="/Store"
             />
              <CardItem
               src="images/img-petNews.jpeg"
               text="Explore all about pet on Article"
-              label="Article"
+              label="Article upComing"
               path="/Article"
             />
              <CardItem
               src="images/Wishbone-Adopt-homepg-2.jpg"
               text="Explore all adoption on Adoption Pet"
-              label="Adoption"
+              label="Adoption upComing"
               path="/Adoption-Pet"
             />
           </ul>
