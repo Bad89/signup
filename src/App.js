c83ea9e7e67cbef3d8045933da25218353f4ec55
bad89 <badeailham@gmail.com>
2022-02-11 22:08:08 +0700

    updateappjs

--- src/App.js
+++ src/App.js
@@ -9,17 +9,22 @@ import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function App() {
   return (
-    <>
+    
+    
       <Router>
         <Navbar />
 
         <Switch>
           <Route path="/" exact component={Home} />
         </Switch>
+        
+        
+        <MessengerCustomerChat pageId="0x5c6532866dbf;" appId="0x1b5e7d2e2f03d;" />
+
       </Router>
 
-      <MessengerCustomerChat pageId="101589709123007" appId="481482256871485" />
-    </>
+      
+    
 
     
   );
